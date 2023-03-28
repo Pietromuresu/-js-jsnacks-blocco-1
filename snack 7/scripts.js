e51152124b50848c614dc269755827454070e99f
Pietromuresu <muresupietro66@gmail.com>
2023-03-28 10:24:28 +0200

    Post-lesson corrections

--- snack 7/scripts.js
+++ snack 7/scripts.js
@@ -1,17 +1,24 @@
 
 const potenze = []
-const limit = (7);
+const limit = 1000;
+let potenza = 1;
 const h1 = document.querySelector('h1')
 
-let potenzeDi2 = 2;
 
-for (let i = 0; i <= limit; i++){
-  potenzeDi2  *= 2;
-  potenze.push(potenzeDi2);
 
+// for (let i = 0; i <= limit; i++){
+//   potenzeDi2  *= 2;
+//   potenze.push(potenzeDi2);
+
+// }
+
+while (potenza < limit) {
+  h1.innerHTML = potenze;
+  potenza *= 2;
+  potenze.push(potenza)
 }
 
-h1.innerHTML = potenze;
+
 
 
 
--- snack 8/scripts.js
+++ snack 8/scripts.js
@@ -1,13 +1,23 @@
 
 const userNumber = prompt('inserisci un numero a 4 cifre')
 
-const numbers = []
-const splittedNumber = userNumber.split('');
-numbers.push(splittedNumber);
+// const numbers = []
+// const splittedNumber = userNumber.split('');
+// numbers.push(splittedNumber);
 
-  console.log(numbers);
+//   console.log(numbers);
 
 
-  const total = parseInt(splittedNumber[0]) + parseInt(splittedNumber[1]) + parseInt(splittedNumber[2]) + parseInt(splittedNumber[3]);  
-  console.log(total);
+//   const total = parseInt(splittedNumber[0]) + parseInt(splittedNumber[1]) + parseInt(splittedNumber[2]) + parseInt(splittedNumber[3]);  
+//   console.log(total);
+ 
 
+let sum = 0;
+
+for (let i = 0; i < userNumber.length; i++ ){
+  sum += parseInt(userNumber[i])
+}
+  console.log(sum);
+
+
+ 
